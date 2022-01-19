68aae85bab9e056ebd2ab86b7f599ddb5799a9c3
shindeanant <shindeanant1997@gmail.com>
2022-01-19 08:11:51 +0530

    Added:Updated Lgoin And Signup Page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { SignupComponent } from './component/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'signupform', component:SignupComponent},
+  {path:'loginform', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
