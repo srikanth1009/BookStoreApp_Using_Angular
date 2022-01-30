fad2b70cd1cc17ee64e45cd94a68dbf0762b8410
shindeanant <shindeanant1997@gmail.com>
2022-01-30 15:39:34 +0530

    Added:Ability to add book to cart.

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -2,17 +2,27 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Token } from '../model/token';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  
+   token : Token = new Token();
+
   base_url=environment.cart_api;
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+
+   }
+
   addToCart(reourceBody: { userId : number; bookId : number; quantity : number; }){
     return this.httpClient.post(`${this.base_url}/add`,reourceBody)
+    
+    
   }
-  getCartDetails() : Observable<any> {
+
+  getCartDetails( ) : Observable<any> {
     return this.httpClient.get(`${this.base_url}/getAll`);
   }
   
